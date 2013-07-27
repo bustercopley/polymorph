3c8c68e7e12401238c7e0931e6b0f75e15fa5098
buster <buster@902d0701-7620-df11-8533-00e0815b4126>
2013-07-27 14:55:39 +0000

    Take a little more time for the morph animaton.

--- src/config.cpp
+++ src/config.cpp
@@ -5,13 +5,13 @@ const unsigned usr::fill_ratio = 100;     // Number of balls per unit of screen 
 // Physical parameters.
 const float usr::min_radius = 1.0f;
 const float usr::max_radius = 1.0f;
-const float usr::density = 100.0f;         // Density of a ball.
+const float usr::density = 100.0f;        // Density of a ball.
 const float usr::walls_friction = 0.075f;
 const float usr::balls_friction = 0.075f;
 const float usr::temperature = 212;       // Average kinetic energy.
 
 // Container characteristics.
-const float usr::tank_distance = 120.0f;   // Distancia del ojo a la pantalla.
+const float usr::tank_distance = 120.0f;  // Distancia del ojo a la pantalla.
 const float usr::tank_depth = 22.0f;      // Tank depth in simulation units.
 const float usr::tank_height = 22.0f;     // Height of screen / tank front.
 
@@ -28,12 +28,12 @@ const float usr::tank_height = 22.0f;     // Height of screen / tank front.
 //        t0        t1    t2        t3      t
 
 //                                         v0     v1     t0     t1     t2     t3
-const bump_specifier_t usr::hsv_v_bump = { 0.25f, 1.00f, 1.50f, 1.75f, 3.25f, 3.75f, };
-const bump_specifier_t usr::hsv_s_bump = { 0.00f, 0.25f, 1.50f, 1.75f, 3.25f, 3.75f, };
+const bump_specifier_t usr::hsv_v_bump = { 0.25f, 1.00f, 1.50f, 1.75f, 3.75f, 4.25f, };
+const bump_specifier_t usr::hsv_s_bump = { 0.00f, 0.25f, 1.50f, 1.75f, 3.75f, 4.25f, };
 
 const float usr::morph_start = 1.75f;
-const float usr::morph_finish = 3.00f;
-const float usr::cycle_duration = 3.75f;
+const float usr::morph_finish = 3.50f;
+const float usr::cycle_duration = 4.25f;
 
 const float usr::specular_material [4] = { 0.3f, 0.3f, 0.3f, 0.0f, };
 
