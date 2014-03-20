c7218f43684b97536afccdaff5d7b2f13d5239f1
buster <buster@902d0701-7620-df11-8533-00e0815b4126>
2014-03-20 19:55:34 +0000

    memory.h: comment out copy_memory definition (cf. revision 294).

--- src/memory.h
+++ src/memory.h
@@ -8,9 +8,9 @@
 void * allocate_internal (std::size_t n);
 void deallocate (void * p);
 
-inline void copy_memory (void * dst, const void * src, std::size_t n)
-{
-  __builtin_memcpy (dst, src, n);
-}
+// inline void copy_memory (void * dst, const void * src, std::size_t n)
+// {
+//   __builtin_memcpy (dst, src, n);
+// }
 
 #endif
