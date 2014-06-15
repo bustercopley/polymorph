1b227c83b47cfb8951330c7a86cae9bfbae1b4c9
buster <buster@902d0701-7620-df11-8533-00e0815b4126>
2014-06-15 00:45:10 +0000

    qpc.h: new inline wrapper for QueryPerformanceCounter.

---
+++ src/qpc.h
@@ -0,0 +1,14 @@
+// -*- C++ -*-
+#ifndef qpc_h
+#define qpc_h
+
+#include "mswin.h"
+
+inline std::uint64_t qpc ()
+{
+  LARGE_INTEGER qpc;
+  ::QueryPerformanceCounter (& qpc);
+  return qpc.QuadPart;
+}
+
+#endif
