880f2a6ffa2d84e0b8320c6edd3ab1776926e118
buster <buster@902d0701-7620-df11-8533-00e0815b4126>
2014-10-12 12:53:35 +0000

    fragment-shader.glsl: shed some brackets and decimal points.

--- src/fragment-shader.glsl
+++ src/fragment-shader.glsl
@@ -8,12 +8,12 @@ out vec4 c;
 
 float amplify (float d)
 {
-  d = clamp (1.0 - 33 * d, 0, 1);
-  return (4./3) * (exp2 (-2 * d * d) - 0.25);
+  d = clamp (1 - 33 * d, 0, 1);
+  return 1.33 * exp2 (-2 * d * d) - .33;
 }
 
 void main ()
 {
   float d = min (min (E [0], E [1]), E [2]);
-  c = vec4 (amplify (d) * N, 1.0);
+  c = vec4 (amplify (d) * N, 1);
 }
