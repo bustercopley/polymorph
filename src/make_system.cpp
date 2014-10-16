c396adaaf21fba94ec343ae94a77a4468c8c5ae4
buster <buster@902d0701-7620-df11-8533-00e0815b4126>
2014-10-16 23:16:07 +0000

    make_system.cpp: P0 and R0 are not needed.

--- src/make_system.cpp
+++ src/make_system.cpp
@@ -5,37 +5,31 @@
 
 void make_system (unsigned q, unsigned r, const float (& xyz_in) [3] [4], float (* nodes) [4], unsigned (* indices) [6])
 {
-  // This is quite a lot to be allocating on the stack.
-  std::uint8_t memory [350];
-
   std::uint8_t * P, * Q, * R; // Permutations taking black triangles around nodes.
   std::uint8_t * Px, * Rx;    // Map a triangle to its P- or R-node.
-  std::uint8_t * P0, * R0;    // Map a P- or R-node to one of its triangles.
 
+  std::uint8_t memory [300];
   std::uint8_t * memp = memory;
   P = memp; memp += 60;
   Q = memp; memp += 60;
   R = memp; memp += 60;
   Px = memp; memp += 60;
-  Rx = memp; memp += 60;
-  P0 = memp; memp += 30;
-  R0 = memp; // memp += 20;
+  Rx = memp; // memp += 60;
 
   const std::uint8_t undef = 0xff;
   const unsigned p = 2, N = 2 * p * q * r / (q * r + r * p + p * q - p * q * r);
-  for (unsigned n = 0; n != 350; ++ n) memory [n] = undef;
+  for (unsigned n = 0; n != sizeof memory; ++ n) memory [n] = undef;
   for (unsigned n = 0; n != N; ++ n) n [Q] = n - n % q + (n + 1) % q;
   for (unsigned n = 0; n != q; ++ n) {
     Px [n] = n;
     Rx [n] = n;
-    P0 [n] = n;
-    R0 [n] = n;
   }
 
-  float (* x) [4] = nodes;
-  float (* y) [4] = x + N / p;
-  float (* z) [4] = y + N / q;
+  float (* const x) [4] = nodes;
+  float (* const y) [4] = x + N / p;
+  float (* const z) [4] = y + N / q;
 
+  // We are given the coordinates of the P-, Q- and R-nodes in triangle 0.
   store4f (x [0], load4f (xyz_in [0]));
   store4f (y [0], load4f (xyz_in [1]));
   store4f (z [0], load4f (xyz_in [2]));
@@ -44,11 +38,11 @@ void make_system (unsigned q, unsigned r, const float (& xyz_in) [3] [4], float 
   float A = two_pi / p;
   float B = two_pi / q;
 
-  // We know the coordinates of the P- and R-nodes around Q-node 0.
-  rotor_t Y_rotate (y [0], B);
-  for (unsigned k = 1; k != q; ++ k) {
-    Y_rotate (x [P0 [k - 1]], x [P0 [k]]);
-    Y_rotate (z [R0 [k - 1]], z [R0 [k]]);
+  // Calculate the coordinates of the P- and R-nodes in the other q-1 triangles around Q-node 0.
+  ALIGNED16 rotor_t Y_rotate (y [0], B);
+  for (unsigned n = 1; n != q; ++ n) {
+    Y_rotate (x [n - 1], x [n]);
+    Y_rotate (z [n - 1], z [n]);
   }
 
   unsigned n0 = 0, p_node = q, r_node = q;
@@ -64,7 +58,7 @@ void make_system (unsigned q, unsigned r, const float (& xyz_in) [3] [4], float 
     // with nodes which are already known (from earlier Q-nodes)
     // and which are new.
 
-    rotor_t X_rotate (x [Px [n0]], A);
+    ALIGNED16 rotor_t X_rotate (x [Px [n0]], A);
     X_rotate (y [n0 / q], y [m0 / q]);
 
     // Work out the consequences of identifying the two P-nodes.
@@ -101,17 +95,15 @@ void make_system (unsigned q, unsigned r, const float (& xyz_in) [3] [4], float 
     // are identified with nodes that are already labelled, so we can give
     // new labels to the remaining nodes and compute their vectors.
 
-    rotor_t Y_rotate (y [m0 / q], B);
+    ALIGNED16 rotor_t Y_rotate (y [m0 / q], B);
     for (unsigned n = m0 + 1; n != m0 + q; ++ n) {
       if (Px [n] == undef) {
         Px [n] = p_node;
-        P0 [p_node] = n;
         ++ p_node;
         Y_rotate (x [Px [n - 1]], x [Px [n]]);
       }
       if (Rx [n] == undef) {
         Rx [n] = r_node;
-        R0 [r_node] = n;
         ++ r_node;
         Y_rotate (z [Rx [n - 1]], z [Rx [n]]);
       }
