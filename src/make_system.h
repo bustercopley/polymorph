13bb8457a598925668d32e281911ba70964aed0d
buster <buster@902d0701-7620-df11-8533-00e0815b4126>
2014-10-17 00:00:47 +0000

    make_system.h: make the argument names in the declaration of make_system consistent with those in the definition.

--- src/make_system.h
+++ src/make_system.h
@@ -3,6 +3,6 @@
 #ifndef make_system_h
 #define make_system_h
 
-void make_system (unsigned q, unsigned r, const float (& xyz_in) [3] [4], float (* xyz) [4], unsigned (* indices) [6]);
+void make_system (unsigned q, unsigned r, const float (& xyz_in) [3] [4], float (* nodes) [4], unsigned (* indices) [6]);
 
 #endif
