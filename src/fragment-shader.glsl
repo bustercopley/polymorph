e383e9a4a6b26680a27cd3e1f717dbde216a68de
buster <buster@902d0701-7620-df11-8533-00e0815b4126>
2015-02-21 17:08:16 +0000

    fragment-shader.glsl: fix fog blending.

--- src/fragment-shader.glsl
+++ src/fragment-shader.glsl
@@ -29,7 +29,7 @@ out vec4 o;
 void main ()
 {
   vec3 H = gl_FrontFacing ? N : -N;
-  vec3 C = a + b;
+  vec3 C = a - b;
   vec3 U = normalize (X);
   for (int i = 0; i != 4; ++ i) {
     vec3 L = normalize (l [i] - X);
@@ -37,5 +37,5 @@ void main ()
     C += d.xyz * F + pow (max (0, dot (L - 2 * F * H, U)), r.w) * r.xyz;
   }
   float e = clamp (f.x + f.y * min (min (min (E [0], E [1]), min (E [2], E [3])), E [4]), 0, 1);
-  o = c + 1.33 * (1 - exp2 (-2 * e * e)) * (vec4 (b + (f.z + f.w * X.z) * (C - b), d.w) - c);
+  o = c + 1.33 * (1 - exp2 (-2 * e * e)) * (vec4 (b + (f.z + f.w * X.z) * C, d.w) - c);
 }
