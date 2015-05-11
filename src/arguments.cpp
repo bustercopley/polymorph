b116d4dd5fdc92d1cf36fe1220fe0f37376efcb6
buster <buster@902d0701-7620-df11-8533-00e0815b4126>
2015-05-11 12:41:56 +0000

    arguments.cpp: handle multiple quote pairs when skipping image path; accept zero or more unexpected characters between components.

--- src/arguments.cpp
+++ src/arguments.cpp
@@ -2,11 +2,13 @@
 #include <cstdint>
 
 #define UPCASE(c) ((c) & ~ TEXT (' '))
+#define ISUPPER(c) (TEXT ('A') <= (c) && (c) <= TEXT ('Z'))
+#define ISDIGIT(c) (TEXT ('0') <= (c) && (c) <= TEXT ('9'))
 
 UINT_PTR number_from_string (const TCHAR * s)
 {
   UINT_PTR result = 0;
-  while (TEXT ('0') <= * s && * s <= TEXT ('9')) {
+  while (ISDIGIT (* s)) {
     result = 10 * result + (* s - TEXT ('0'));
     ++ s;
   }
@@ -15,31 +17,28 @@ UINT_PTR number_from_string (const TCHAR * s)
 
 void get_arguments (const TCHAR * s, arguments_t & args)
 {
-  // Default to configure mode if the mode letter is
-  // missing or unrecognised.
-  args.mode = configure;
-
   // Skip the image path, which might be quoted.
-  if (* s == TEXT ('"')) {
-    ++ s;
-    while (* s && * s != TEXT ('"')) ++ s;
-  }
-  while (* s && * s != TEXT (' ')) ++ s;
+  TCHAR c;
+  bool quoted = false;
+  while (c = * s, quoted ^= c == TEXT ('"'), c && (quoted || c != TEXT (' '))) ++ s;
 
-  // Skip zero or more spaces.
-  while (* s == TEXT (' ')) ++ s;
+  // Skip zero or more non-alphanumeric characters.
+  c = TEXT ('\0');
+  while (* s && (c = UPCASE (* s), ! (ISUPPER (c) || ISDIGIT (c)))) ++ s;
 
   // Read optional mode letter.
-  TCHAR c = UPCASE (* s);
-  if (TEXT ('A') <= c && c <= TEXT ('Z')) {
+  // Default to configure mode if the mode letter is missing or unrecognised.
+  args.mode = configure;
+  if (ISUPPER (c)) {
     ++ s;
     if (c == TEXT ('S')) args.mode = screensaver;
     else if (c == TEXT ('X')) args.mode = persistent;
     else if (c == TEXT ('P') || c == TEXT ('L')) args.mode = parented;
+    // Any other letter means configure mode.
   }
 
-  // Skip zero or more spaces or colons.
-  while (* s == TEXT (' ') || * s == TEXT (':')) ++ s;
+  // Skip zero or more non-numeric characters.
+  while (* s && ! ISDIGIT (* s)) ++ s;
 
   // Read optional numeric argument.
   args.numeric_arg = number_from_string (s);
