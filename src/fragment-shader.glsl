deeb9633a2560086ed493c114702ddb5d53a5ffe
buster <buster@902d0701-7620-df11-8533-00e0815b4126>
2016-11-27 11:42:52 +0000

    fragment-shader.glsl: don't reuse uniform-block names as variable names because it makes Intel HD Graphics drivers fall over.

--- src/fragment-shader.glsl
+++ src/fragment-shader.glsl
@@ -28,13 +28,13 @@ out vec4 o;
 
 void main ()
 {
-  vec3 H = gl_FrontFacing ? N : -N;
+  vec3 G = gl_FrontFacing ? N : -N;
   vec3 C = a - b;
   vec3 U = normalize (X);
   for (int i = 0; i != 4; ++ i) {
     vec3 L = normalize (l [i] - X);
-    float F = max (0, dot (L, H));
-    C += d.xyz * F + pow (max (0, dot (L - 2 * F * H, U)), r.w) * r.xyz;
+    float E = max (0, dot (L, G));
+    C += d.xyz * E + pow (max (0, dot (L - 2 * E * G, U)), r.w) * r.xyz;
   }
   float e = clamp (f.x + f.y * min (min (min (E [0], E [1]), min (E [2], E [3])), E [4]), 0, 1);
   o = c + 1.33 * (1 - exp2 (-2 * e * e)) * (vec4 (b + (f.z + f.w * X.z) * C, d.w) - c);
