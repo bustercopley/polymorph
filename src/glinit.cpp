026b18c498ee6ddbdcb2ef42972dae48441c6e5d
Buster <buster@buster.me.uk>
2018-09-23 20:22:15 +0100

    glinit.cpp (pf_attribs): Request OpenGL 4.3.
    We already use "#version 430" in shaders, which is the minimum
    required (for multidimensional arrays).

--- src/glinit.cpp
+++ src/glinit.cpp
@@ -44,7 +44,7 @@ const int pf_attribs [] = {
 // Rendering context attributes for up-level opengl context.
 const int context_attribs [] = {
   WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
-  WGL_CONTEXT_MINOR_VERSION_ARB, 2,
+  WGL_CONTEXT_MINOR_VERSION_ARB, 3,
   WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
   0, 0,
 };
