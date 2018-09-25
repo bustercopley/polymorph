a04d95ce7d4ef0e7ad8c61efce9f404a0e66d827
Buster <buster@buster.me.uk>
2018-09-25 19:41:47 +0100

    aligned_arrays.h (reallocate_aligned_arrays): Use a fold expression.
    Delete function internal::sum_sizes.

--- src/aligned-arrays.h
+++ src/aligned-arrays.h
@@ -36,16 +36,6 @@ namespace internal
     return (T) ((((std::intptr_t) p) + alignment - 1) & -alignment);
   }
 
-  inline constexpr std::size_t sum_sizes ()
-  {
-    return 0;
-  }
-
-  template <typename T, typename ... P> inline constexpr std::size_t sum_sizes (T **, P ... ps)
-  {
-    return sizeof (T) + sum_sizes (ps ...);
-  }
-
   inline void setp (std::size_t, char *)
   {
     // Base case for recursion.
@@ -60,12 +50,12 @@ namespace internal
 }
 
 template <typename ... P>
-inline void reallocate_aligned_arrays (void * & memory, std::size_t & capacity, std::size_t new_capacity, P ... p)
+inline void reallocate_aligned_arrays (void * & memory, std::size_t & capacity, std::size_t new_capacity, P** ... p)
 {
   if (new_capacity > capacity) {
     deallocate (memory);
     capacity = internal::align_up (new_capacity, 16);
-    std::size_t bytes_needed = capacity * internal::sum_sizes (p ...) + 63;
+    std::size_t bytes_needed = capacity * (0 + ... + sizeof (P)) + 63;
     memory = allocate (bytes_needed);
     void * memp = internal::align_up (memory, 64);
     internal::setp (capacity, (char *) memp, p ...);
