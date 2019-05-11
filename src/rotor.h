c891947231b0ed91e4b581be927cc9420de2d1c5
Buster <buster@buster.me.uk>
2019-05-11 17:29:36 +0100

    rotor.h: Fit to 80 columns.

--- src/rotor.h
+++ src/rotor.h
@@ -22,7 +22,9 @@
 struct rotor_t
 {
   rotor_t (const float (& u) [4], float A);
-  void operator () (const float (& in) [4], float (& out) [4]) const;
+  void operator () (const float (& in) [4], float (& out) [4]) const {
+    store4f (out, mapply (matrix, load4f (in)));
+  }
 private:
   float matrix [4] [4];
 };
@@ -34,11 +36,6 @@ inline rotor_t::rotor_t (const float (& u) [4], float a)
   store4f (U, _mm_set1_ps (a) * _mm_load_ps (u));
   const unsigned permutation [1] = { 0 };
   compute ((char *) & matrix, 0, & X, & U, permutation, 1);
-}
-
-inline void rotor_t::operator () (const float (& in) [4], float (& out) [4]) const
-{
-  store4f (out, mapply (matrix, load4f (in)));
 }
 
 #endif
