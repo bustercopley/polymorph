18a3aa44bdee6e3f4e0c5871fa7ecdaff9bbac28
Buster <buster@buster.me.uk>
2019-05-11 16:27:33 +0100

    model.cpp: Refill and fit to 80 columns.

--- src/model.cpp
+++ src/model.cpp
@@ -69,9 +69,9 @@ namespace usr {
   // v0 +XXXX----------+----+----------XXXX-->
   //        t0        t1    t2        t3      t
 
-  //                                      t0     t1     t2     t3     v0     v1
-  const bump_specifier_t hsv_s_bump = { 1.50f, 1.75f, 3.75f, 4.25f, 0.00f, 0.275f, };
-  const bump_specifier_t hsv_v_bump = { 1.50f, 1.75f, 3.75f, 4.25f, 0.09f, 0.333f, };
+  //                                 t0     t1     t2     t3     v0     v1
+  const bump_specifier_t sbump = { 1.50f, 1.75f, 3.75f, 4.25f, 0.00f, 0.275f };
+  const bump_specifier_t vbump = { 1.50f, 1.75f, 3.75f, 4.25f, 0.09f, 0.333f };
 
   // Parameters for morph animation timings.
   const float morph_start = 1.75f;
@@ -137,8 +137,12 @@ inline float rainbow_hue (float x)
   // Seen as a graph of (angular position)/2pi vs. time, f(x) describes
   // a one-revolution rotation at variable speed over one unit of time.
 
-  const v4f poly_lo = { +0x1.f2b75cP-1f, +0x1.d75e6cP-9f, +0x1.55b804P+3f, -0x1.0a7118P+6f, };
-  const v4f poly_hi = { +0x1.811250P+7f, -0x1.1565f6P+8f, +0x1.86704aP+7f, -0x1.ab6de6P+5f, };
+  const v4f poly_lo = {
+    +0x1.f2b75cP-1f, +0x1.d75e6cP-9f, +0x1.55b804P+3f, -0x1.0a7118P+6f
+  };
+  const v4f poly_hi = {
+    +0x1.811250P+7f, -0x1.1565f6P+8f, +0x1.86704aP+7f, -0x1.ab6de6P+5f
+  };
   x -= (int) x; // Fractional part, assuming non-negative.
   return polyeval7 (x, poly_lo, poly_hi);
 }
@@ -195,8 +199,9 @@ bool model_t::start (int width, int height, const settings_t & settings)
   float phase_offset = get_float (rng, 1.0f, 2.0f);
   // Trackbar positions 0, 1, 2 specify 1, 2, 3 objects respectively;
   // subsequently the number of objects increases linearly with position.
-  unsigned max_count = std::max (3u, truncate (usr::fill_factor * (x1 * y1 * zd) / (rsq * radius)));
-  DWORD pos = settings.trackbar_pos [0];
+  unsigned max_count = std::max (3u,
+    truncate (usr::fill_factor * (x1 * y1 * zd) / (rsq * radius)));
+  DWORD pos = settings.trackbar_pos[0];
   count = pos < 2 ? pos + 1 : 3 + (max_count - 3) * (pos - 2) / 98;
   set_capacity (count);
 
@@ -236,61 +241,63 @@ bool model_t::start (int width, int height, const settings_t & settings)
     // Initial animation_time is in [T, 2T) to force an immediate transition.
     A.animation_time = (1.0f + phase) * usr::cycle_duration;
 
-    // Get two independent random integers, d6 uniform on [0, 6) and d8 uniform on [0, 8).
+    // Get independent integers, d6 uniform on [0, 6) and d8 uniform on [0, 8).
     std::uint32_t d = (std::uint32_t) rng.get ();
     std::uint32_t d8 = d & 7u;
-    // Need at least 2 high bits zero to avoid overflow. Discard the 3 bits used for d8.
     std::uint32_t d6 = (3u * (d >> 3u)) >> 28u;
     A.target.system = (system_select_t) d6;
     A.target.point = d8;
     A.starting_point = A.target.point;
   }
 
-  // Take the animation-speed setting, s, a whole number in the range 0 to 100, inclusive;
-  // every frame, the morph/fade animation time is advanced by the time interval kT, where
-  // k is an increasing continuous function of s, and the constant T is the default frame time.
+  // Take the animation-speed s, an integer in the range 0 to 100, inclusive;
+  // every frame, the morph/fade animation time is advanced by the time interval
+  // kT, where k is an increasing continuous function of s, and the constant T
+  // is the default frame time.
   DWORD s = settings.trackbar_pos [2];
   float k = 0.02f * ui2f (s);  // 0.0 <= k <= 2.0
   // Boost animation speed in the upper half of the range.
   animation_speed_constant = s <= 50 ? k : ((k * k) * (k * k));
 
-  // Initialize bump function object for the lightness and saturation fading animation.
-  ALIGNED16 bump_specifier_t s_bump = usr::hsv_s_bump;
-  ALIGNED16 bump_specifier_t v_bump = usr::hsv_v_bump;
+  // Initialize bump functions for the lightness and saturation fade animation.
+  ALIGNED16 bump_specifier_t sbump = usr::sbump;
+  ALIGNED16 bump_specifier_t vbump = usr::vbump;
   if (s >= 75) {
     // No fading at all above 75% animation speed.
-    s_bump.v0 = s_bump.v1;
-    v_bump.v0 = v_bump.v1;
+    sbump.v0 = sbump.v1;
+    vbump.v0 = vbump.v1;
   }
   else if (s >= 50) {
     // Between 50% and 75% animation speed, progressively suppress fading.
     float g = 2.0f - k;      // Saturation fading decreases linearly.
     float h = cube (g);      // Lightness fading falls off more rapidly.
-    s_bump.v0 = g * s_bump.v0 + (1.0f - g) * s_bump.v1;
-    v_bump.v0 = h * v_bump.v0 + (1.0f - h) * v_bump.v1;
+    sbump.v0 = g * sbump.v0 + (1.0f - g) * sbump.v1;
+    vbump.v0 = h * vbump.v0 + (1.0f - h) * vbump.v1;
   }
   if (s <= 30) {
     // At low animation speed, offset the attack-begin and attack-end
     // times of the lightness and saturation independently to give a
     // white warning flash effect.
-    s_bump.t0 += 0.10f;
-    s_bump.t1 += 0.25f;
-    v_bump.t1 -= 0.15f;
+    sbump.t0 += 0.10f;
+    sbump.t1 += 0.25f;
+    vbump.t1 -= 0.15f;
   }
-  bumps.initialize (s_bump, v_bump);
+  bumps.initialize (sbump, vbump);
 
   // Allow the balls to jostle for space.
   for (unsigned n = 0; n != 24; ++ n) nodraw_next ();
 
   // Slow down to the configured speed.
   s = settings.trackbar_pos [1];
-  v4f speedup = _mm_set1_ps (ui2f (s) * (s <= 50 ? (0.125f / 50) : (0.125f / (50 * 50)) * ui2f (s)));
-  for (unsigned n = 0; n != count; ++ n) {
+  v4f speedup = _mm_set1_ps (
+    ui2f (s) * (s <= 50 ? (0.125f / 50) : (0.125f / (50 * 50)) * ui2f (s)));
+  for (unsigned n = 0; n != count; ++n) {
     store4f (v [n], speedup * load4f (v [n]));
     store4f (w [n], speedup * load4f (w [n]));
   }
 
-  // Sort in reverse depth order for painter's algorithm (maintained with insertion_sort in draw_next).
+  // Sort in reverse depth order for painter's algorithm (maintained with
+  // insertion_sort in draw_next).
   qsort (object_order, x, 2, 0, count);
 
   return true;
@@ -308,8 +315,9 @@ bool model_t::initialize (std::uint64_t seed)
 model_t::~model_t ()
 {
 #if PRINT_ENABLED
-  std::cout << std::scientific << std::setprecision (8);
-  std::cout << "Required range for arccos function: x in [" << min_d << ", " << max_d << "].\n\n";
+  std::cout << std::scientific << std::setprecision (8)
+            << "Required range for arccos function: x in [" << min_d << ", "
+            << max_d << "].\n\n";
 
   double total_polyhedron_count = 0.0;
   for (unsigned n = 0; n != 18; ++ n) {
@@ -318,7 +326,9 @@ model_t::~model_t ()
 
   std::cout << std::fixed << std::setprecision (2);
   for (unsigned n = 0; n != 18; ++ n) {
-    std::cout << std::setw (10) << (100.0f * polyhedron_counts [n] / total_polyhedron_count) << " % " << names [n] << "\n";
+    std::cout << std::setw (10)
+              << (100.0f * polyhedron_counts[n] / total_polyhedron_count)
+              << " % " << names[n] << "\n";
   }
 #endif
   //deallocate (memory);
@@ -327,12 +337,8 @@ model_t::~model_t ()
 
 void model_t::set_capacity (std::size_t new_capacity)
 {
-  reallocate_aligned_arrays (memory, capacity, new_capacity,
-                             x, v, u, w, e,
-                             kdtree_index,
-                             kdtree_aux,
-                             objects,
-                             object_order);
+  reallocate_aligned_arrays (memory, capacity, new_capacity, x, v, u, w, e,
+    kdtree_index, kdtree_aux, objects, object_order);
 }
 
 void model_t::recalculate_locus (unsigned index)
@@ -341,18 +347,29 @@ void model_t::recalculate_locus (unsigned index)
   float (& locus_end) [4] = e [index];
 
   system_select_t sselect = object.target.system;
-  v4f g0 = load4f (abc [sselect] [object.starting_point]); // g0: Coefficients for T0 in terms of xyz.
-  v4f g1 = load4f (abc [sselect] [object.target.point]);   // g1: Coefficients for T1 in terms of xyz.
-  v4f T0 = mapply (xyz [sselect], g0);                     // T0: Beginning of locus (unit vector).
-  v4f T1 = mapply (xyz [sselect], g1);                     // T1: End of locus (also a unit vector).
-  // Find a unit vector T2 perpendicular to T0 and and an angle s such that T1 = (cos s) T0 + (sin s) T2.
-  v4f d = dot (T0, T1);                                    // d:  Dot product of T0 and T1 (i.e., cos s, where s is the arc-length T0-T1).
-  v4f T2 = normalize (T1 - d * T0);                        // T2: Take the component of T1 perpendicular to T0, then normalize.
-  v4f g2 = mapply (xyzinv [sselect], T2);                  // g2: Coefficents for T2 in terms of xyz.
-  // Any point T = (cos t) T0 + (sin t) T2 (where 0 <= t <= s) on the hemisemicircular arc T0-T1 has coefficients (cos t) g0 + (sin t) g2.
+
+  // g0: Coefficients for T0 in terms of xyz.
+  // g1: Coefficients for T1 in terms of xyz.
+  // T0: Beginning of locus (unit vector).
+  // T1: End of locus (also a unit vector).
+  // d:  Dot product of T0 and T1, i.e., cos s where s is the arc length T0-T1.
+  // T2: Component of T1 perpendicular to T0, normalized to a unit vector.
+  // g2: Coefficents for T2 in terms of xyz.
+
+  v4f g0 = load4f (abc [sselect] [object.starting_point]);
+  v4f g1 = load4f (abc [sselect] [object.target.point]);
+  v4f T0 = mapply (xyz [sselect], g0);
+  v4f T1 = mapply (xyz [sselect], g1);
+  // Find a unit vector T2 perpendicular to T0 and and an angle s such that
+  // T1 = (cos s) T0 + (sin s) T2.
+  v4f d = dot (T0, T1);
+  v4f T2 = normalize (T1 - d * T0);
+  v4f g2 = mapply (xyzinv [sselect], T2);
+  // Any point T = (cos t) T0 + (sin t) T2 (where 0 <= t <= s) on the
+  // hemisemicircular arc T0-T1 has coefficients (cos t) g0 + (sin t) g2.
   store4f (locus_end, g2);
   // Calculate the arc-length s = arccos(d) of the great circular arc T0-T1.
-  // Note: for allowed transitions we have d in [7.74596691e-001, 9.90879238e-001].
+  // Note: for allowed transitions we have d in [0.774596691, 0.990879238].
   object.locus_length = _mm_cvtss_f32 (arccos (d));
 
 #if PRINT_ENABLED
@@ -391,7 +408,8 @@ void model_t::draw_next ()
       transition (rng, u [n], A.target, A.starting_point);
       recalculate_locus (n);
 #if PRINT_ENABLED
-      ++ polyhedron_counts [polyhedra [(int) A.target.system] [A.target.point] - 1];
+      int wenninger_num = polyhedra [(int) A.target.system] [A.target.point];
+      ++ polyhedron_counts [wenninger_num - 1];
 #endif
     }
     A.animation_time = t;
@@ -421,39 +439,41 @@ void model_t::draw (unsigned begin, unsigned count)
   uniform_buffer_t & uniform_buffer = program.uniform_buffer;
 
   // Set the modelview matrix, m.
-  compute (reinterpret_cast <char *> (& uniform_buffer [0].m), uniform_buffer.stride (), x, u, & (object_order [begin]), count);
+  compute (reinterpret_cast <char *> (& uniform_buffer [0].m),
+    uniform_buffer.stride (), x, u, & (object_order [begin]), count);
 
   const v4f alpha = { 0.0f, 0.0f, 0.0f, usr::alpha, };
   for (unsigned n = 0; n != count; ++ n) {
     unsigned m = object_order [begin + n];
-    const object_t & object = objects [m];
+    const object_t & obj = objects [m];
     object_data_t & block = uniform_buffer [n];
 
     // Snub?
-    block.s = (GLuint) (object.starting_point == 7 || object.target.point == 7);
+    block.s = (GLuint) (obj.starting_point == 7 || obj.target.point == 7);
 
     // Set the diffuse material reflectance, d.
-    v4f satval = bumps (object.animation_time);
+    v4f satval = bumps (obj.animation_time);
     v4f sat = _mm_moveldup_ps (satval);
     v4f val = _mm_movehdup_ps (satval);
-    _mm_stream_ps (block.d, hsv_to_rgb (object.hue, sat, val, alpha));
+    _mm_stream_ps (block.d, hsv_to_rgb (obj.hue, sat, val, alpha));
 
     // Set the vertex coefficients g.
-    system_select_t sselect = object.target.system;
-    v4f t = step (object.animation_time) * _mm_set1_ps (object.locus_length);
+    system_select_t sselect = obj.target.system;
+    v4f t = step (obj.animation_time) * _mm_set1_ps (obj.locus_length);
     v4f sc = sincos (t);
     v4f s = _mm_moveldup_ps (sc);
     v4f c = _mm_movehdup_ps (sc);
-    v4f g = c * load4f (abc [sselect] [object.starting_point]) + s * load4f (e [m]);
-    _mm_stream_ps (block.g, _mm_set1_ps (object.r) * g);
+    v4f g0 = load4f (abc [sselect] [obj.starting_point]);
+    v4f g = c * g0 + s * load4f (e [m]);
+    _mm_stream_ps (block.g, _mm_set1_ps (obj.r) * g);
   }
 
   uniform_buffer.update ();
 
   for (unsigned n = 0; n != count; ++ n) {
     unsigned m = object_order [begin + n];
-    const object_t & object = objects [m];
-    system_select_t sselect = object.target.system;
+    const object_t & obj = objects [m];
+    system_select_t sselect = obj.target.system;
 
     paint (primitive_count [sselect],
            vao_ids [sselect],
