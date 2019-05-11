26c3299abac650f6744ff4be0f715034d59390b7
Buster <buster@buster.me.uk>
2019-05-11 17:50:15 +0100

    systems.h: Refill and fit to 80 columns.

--- src/systems.cpp
+++ src/systems.cpp
@@ -25,8 +25,8 @@
 
 struct triangle_t
 {
-  float xyz [3] [4]; // Coordinates for a single triangle XYZ of the Moebius tiling.
-  float abc [8] [4]; // Coefficients A, B, C for the generator T = AX + BY + CZ.
+  float xyz [3] [4]; // Coordinates for single triangle XYZ of Moebius tiling.
+  float abc [8] [4]; // Coefficients A, B, C for generator T = AX + BY + CZ.
 };
 
 // The fundamental triangle of the tiling has angles
@@ -48,14 +48,14 @@ struct triangle_t
 // For certain triples, replicating the point T in each triangle
 // of the tiling gives the vertices of a uniform polyhedron:
 
-// { 1, 0, 0, },             Point X.
-// { 0, 1, 0, },             Point Y.
-// { 0, 0, 1, },             Point Z.
-// { 0, sinb, sinc, },       Intersection of YZ with the angular bisector of corner X.
-// { sina, 0, sinc, },       Intersection of ZX with the angular bisector of corner Y.
-// { sina, sinb, 0, },       Intersection of YZ with the angular bisector of corner Z.
-// { sinA, sinB, sinC, },    Incentre (intersection of the three bisectors).
-// { alpha, beta, gamma, },  Snub generator.
+// { 1, 0, 0 },             Point X.
+// { 0, 1, 0 },             Point Y.
+// { 0, 0, 1 },             Point Z.
+// { 0, sinb, sinc },       Intersection of YZ with bisector of angle X.
+// { sina, 0, sinc },       Intersection of ZX with bisector of angle Y.
+// { sina, sinb, 0 },       Intersection of YZ with bisector of angle Z.
+// { sinA, sinB, sinC },    Incentre (intersection of the three bisectors).
+// { alpha, beta, gamma },  Snub generator.
 
 //     X1
 //    /  \    Part of the tiling of the sphere, showing
@@ -74,52 +74,53 @@ struct triangle_t
 const ALIGNED16 triangle_t triangles [3] = {
   // Tetrahedral, <2, 3, 3>.
   {
-    { { +0x1.000000P+000f, +0x0.000000P+000f, +0x0.000000P+000f, 0.0f, },
-      { +0x1.279a74P-001f, +0x1.a20bd8P-001f, +0x0.000000P+000f, 0.0f, },
-      { +0x1.279a74P-001f, +0x0.000000P+000f, +0x1.a20bd8P-001f, 0.0f, },
+    { { +0x1.000000P+0f, +0x0.000000P+0f, +0x0.000000P+0f, 0.0f },
+      { +0x1.279a74P-1f, +0x1.a20bd8P-1f, +0x0.000000P+0f, 0.0f },
+      { +0x1.279a74P-1f, +0x0.000000P+0f, +0x1.a20bd8P-1f, 0.0f },
     },
-    { { +0x1.000000P+000f, +0x0.000000P+000f, +0x0.000000P+000f, 0.0f, }, // Octahedron
-      { +0x0.000000P+000f, +0x1.000000P+000f, +0x0.000000P+000f, 0.0f, }, // Tetrahedron
-      { +0x0.000000P+000f, +0x0.000000P+000f, +0x1.000000P+000f, 0.0f, }, // Tetrahedron
-      { +0x0.000000P+000f, +0x1.3988e2P-001f, +0x1.3988e2P-001f, 0.0f, }, // Cuboctahedron
-      { +0x1.34bf64P-001f, +0x0.000000P+000f, +0x1.0b621eP-001f, 0.0f, }, // Truncated tetrahedron
-      { +0x1.34bf64P-001f, +0x1.0b621eP-001f, +0x0.000000P+000f, 0.0f, }, // Truncated tetrahedron
-      { +0x1.c9f25cP-002f, +0x1.8c97f0P-002f, +0x1.8c97f0P-002f, 0.0f, }, // Truncated octahedron
-      { +0x1.4cb7c0P-002f, +0x1.d2393eP-002f, +0x1.d2393eP-002f, 0.0f, }, // Icosahedron
-      // { -0x1.605a90P+000f, +0x1.792eceP-001f, +0x1.792eceP-001f, 0.0f, }, // Great icosahedron
+    { { +0x1.000000P+0f, +0x0.000000P+0f, +0x0.000000P+0f, 0.0f }, // 2
+      { +0x0.000000P+0f, +0x1.000000P+0f, +0x0.000000P+0f, 0.0f }, // 1
+      { +0x0.000000P+0f, +0x0.000000P+0f, +0x1.000000P+0f, 0.0f }, // 1
+      { +0x0.000000P+0f, +0x1.3988e2P-1f, +0x1.3988e2P-1f, 0.0f }, // 11
+      { +0x1.34bf64P-1f, +0x0.000000P+0f, +0x1.0b621eP-1f, 0.0f }, // 6
+      { +0x1.34bf64P-1f, +0x1.0b621eP-1f, +0x0.000000P+0f, 0.0f }, // 6
+      { +0x1.c9f25cP-2f, +0x1.8c97f0P-2f, +0x1.8c97f0P-2f, 0.0f }, // 7
+      { +0x1.4cb7c0P-2f, +0x1.d2393eP-2f, +0x1.d2393eP-2f, 0.0f }, // 4
+      // Great icosahedron:
+      // { -0x1.605a90P+0f, +0x1.792eceP-1f, +0x1.792eceP-1f, 0.0f },
     },
   },
   // Octahedral, <2, 4, 3>.
   // Oriented so that two octahedral triangles cover one tetrahedral triangle.
   {
-    { { +0x1.6a09e6P-001f, +0x1.000000P-001f, +0x1.000000P-001f, 0.0f, },
-      { +0x1.000000P+000f, +0x0.000000P+000f, +0x0.000000P+000f, 0.0f, },
-      { +0x1.279a74P-001f, +0x1.a20bd8P-001f, +0x0.000000P+000f, 0.0f, },
+    { { +0x1.6a09e6P-1f, +0x1.000000P-1f, +0x1.000000P-1f, 0.0f },
+      { +0x1.000000P+0f, +0x0.000000P+0f, +0x0.000000P+0f, 0.0f },
+      { +0x1.279a74P-1f, +0x1.a20bd8P-1f, +0x0.000000P+0f, 0.0f },
     },
-    { { +0x1.000000P+000f, +0x0.000000P+000f, +0x0.000000P+000f, 0.0f, }, // Cuboctahedron
-      { +0x0.000000P+000f, +0x1.000000P+000f, +0x0.000000P+000f, 0.0f, }, // Octahedron
-      { +0x0.000000P+000f, +0x0.000000P+000f, +0x1.000000P+000f, 0.0f, }, // Cube
-      { +0x0.000000P+000f, +0x1.02ca46P-001f, +0x1.3cf3aeP-001f, 0.0f, }, // Rhombicuboctahedron
-      { +0x1.1fd4a6P-001f, +0x0.000000P+000f, +0x1.f28990P-002f, 0.0f, }, // Truncated cube
-      { +0x1.43d136P-001f, +0x1.c9f25cP-002f, +0x0.000000P+000f, 0.0f, }, // Truncated octahedron
-      { +0x1.b9d594P-002f, +0x1.386c8eP-002f, +0x1.7ea3c6P-002f, 0.0f, }, // Rhombitruncated cuboctahedron
-      { +0x1.31816eP-002f, +0x1.8d5502P-002f, +0x1.bdd092P-002f, 0.0f, }, // Snub cube
+    { { +0x1.000000P+0f, +0x0.000000P+0f, +0x0.000000P+0f, 0.0f }, // 11
+      { +0x0.000000P+0f, +0x1.000000P+0f, +0x0.000000P+0f, 0.0f }, // 2
+      { +0x0.000000P+0f, +0x0.000000P+0f, +0x1.000000P+0f, 0.0f }, // 3
+      { +0x0.000000P+0f, +0x1.02ca46P-1f, +0x1.3cf3aeP-1f, 0.0f }, // 13
+      { +0x1.1fd4a6P-1f, +0x0.000000P+0f, +0x1.f28990P-2f, 0.0f }, // 8
+      { +0x1.43d136P-1f, +0x1.c9f25cP-2f, +0x0.000000P+0f, 0.0f }, // 7
+      { +0x1.b9d594P-2f, +0x1.386c8eP-2f, +0x1.7ea3c6P-2f, 0.0f }, // 15
+      { +0x1.31816eP-2f, +0x1.8d5502P-2f, +0x1.bdd092P-2f, 0.0f }, // 17
     },
   },
   // Icosahedral, <2, 5, 3>.
   {
-    { { +0x1.000000P+000f, +0x0.000000P+000f, +0x0.000000P+000f, 0.0f, },
-      { +0x1.b38880P-001f, +0x1.0d2ca0P-001f, +0x0.000000P+000f, 0.0f, },
-      { +0x1.de4bd6P-001f, +0x0.000000P+000f, +0x1.6d62c6P-002f, 0.0f, },
+    { { +0x1.000000P+0f, +0x0.000000P+0f, +0x0.000000P+0f, 0.0f },
+      { +0x1.b38880P-1f, +0x1.0d2ca0P-1f, +0x0.000000P+0f, 0.0f },
+      { +0x1.de4bd6P-1f, +0x0.000000P+0f, +0x1.6d62c6P-2f, 0.0f },
     },
-    { { +0x1.000000P+000f, +0x0.000000P+000f, +0x0.000000P+000f, 0.0f, }, // Icosidodecahedron
-      { +0x0.000000P+000f, +0x1.000000P+000f, +0x0.000000P+000f, 0.0f, }, // Icosahedron
-      { +0x0.000000P+000f, +0x0.000000P+000f, +0x1.000000P+000f, 0.0f, }, // Dodecahedron
-      { +0x0.000000P+000f, +0x1.b4242aP-002f, +0x1.414c80P-001f, 0.0f, }, // Rhombicosidodecahedron
-      { +0x1.16fc4eP-001f, +0x0.000000P+000f, +0x1.e33798P-002f, 0.0f, }, // Truncated dodecahedron
-      { +0x1.4e5014P-001f, +0x1.890220P-002f, +0x0.000000P+000f, 0.0f, }, // Truncated icosahedron
-      { +0x1.b3be36P-002f, +0x1.001f92P-002f, +0x1.795d50P-002f, 0.0f, }, // Rhombitruncated icosidodecahedron
-      { +0x1.287f1eP-002f, +0x1.52a52eP-002f, +0x1.b882c6P-002f, 0.0f, }, // Snub dodecahedron
+    { { +0x1.000000P+0f, +0x0.000000P+0f, +0x0.000000P+0f, 0.0f }, // 12
+      { +0x0.000000P+0f, +0x1.000000P+0f, +0x0.000000P+0f, 0.0f }, // 4
+      { +0x0.000000P+0f, +0x0.000000P+0f, +0x1.000000P+0f, 0.0f }, // 5
+      { +0x0.000000P+0f, +0x1.b4242aP-2f, +0x1.414c80P-1f, 0.0f }, // 14
+      { +0x1.16fc4eP-1f, +0x0.000000P+0f, +0x1.e33798P-2f, 0.0f }, // 10
+      { +0x1.4e5014P-1f, +0x1.890220P-2f, +0x0.000000P+0f, 0.0f }, // 9
+      { +0x1.b3be36P-2f, +0x1.001f92P-2f, +0x1.795d50P-2f, 0.0f }, // 16
+      { +0x1.287f1eP-2f, +0x1.52a52eP-2f, +0x1.b882c6P-2f, 0.0f }, // 18
     },
   },
 };
@@ -132,8 +133,8 @@ void reflect (float (& abc) [8] [4],
   v4f y = load4f (xyz [1]);
   v4f z = load4f (xyz [2]);
 
-  v4f n = cross (x, y);
-  v4f d = (dot (n, z) / dot (n, n)) * n; // Component of z perpendicular to x and y.
+  v4f n = cross (x, y);                  // Perpendicular to x and y.
+  v4f d = (dot (n, z) / dot (n, n)) * n; // Component of z parallel to n.
 
   store4f (xyz [1], z - (d + d));
   store4f (xyz [2], y);
@@ -145,16 +146,16 @@ void reflect (float (& abc) [8] [4],
 }
 
 void initialize_systems (float (& abc) [system_count] [8] [4],
-                         float (& xyz) [system_count] [3] [4],
-                         float (& xyzinv) [system_count] [3] [4],
-                         unsigned (& primitive_count) [system_count],
-                         unsigned (& vao_ids) [system_count])
+  float (& xyz) [system_count] [3] [4], float (& xyzinv) [system_count] [3] [4],
+  unsigned (& primitive_count) [system_count],
+  unsigned (& vao_ids) [system_count])
 {
   ALIGNED16 float nodes [62] [4];
   std::uint8_t indices [60] [6];
 
-  auto init = [&] (const triangle_t & t, system_select_t select, unsigned q, unsigned r, bool reflected) -> void
-  {
+  auto init = [&](const triangle_t & t, system_select_t select, unsigned q,
+    unsigned r, bool reflected) -> void {
+
     std::memcpy (xyz [select], t.xyz, sizeof t.xyz);
     std::memcpy (abc [select], t.abc, sizeof t.abc);
     if (reflected) {
