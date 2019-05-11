d937d8bcd680d6dd50505fd615fd0cb9a82988e4
Buster <buster@buster.me.uk>
2019-05-11 14:28:11 +0100

    cramer.h: Delete a chunk of comments to fit 80 columns.

--- src/cramer.h
+++ src/cramer.h
@@ -42,9 +42,9 @@ void cramer::inverse (const float (& m) [3] [4], float (& minv) [3] [4])
   v4f m2s = _mm_shuffle_ps (m2, m2, SHUFFLE (1, 2, 0, 3)); // m21 m22 m20 0
 
   // Shuffled cofactors.
-  v4f c0s = m1 * m2s - m2 * m1s; // m10m21-m20m11 m11m22-m21m12 m12m20-m22m10 0 = c02 c00 c01 0
-  v4f c1s = m2 * m0s - m0 * m2s; // m20m01-m00m21 m21m02-m01m22 m22m00-m02m20 0 = c12 c10 c11 0
-  v4f c2s = m0 * m1s - m1 * m0s; // m00m11-m10m01 m01m12-m11m02 m02m10-m12m00 0 = c22 c20 c21 0
+  v4f c0s = m1 * m2s - m2 * m1s; // c02 c00 c01 0
+  v4f c1s = m2 * m0s - m0 * m2s; // c12 c10 c11 0
+  v4f c2s = m0 * m1s - m1 * m0s; // c22 c20 c21 0
 
   // Row 0 of the cofactor matrix.
   v4f c0r = _mm_shuffle_ps (c0s, c0s, SHUFFLE (1, 2, 0, 3)); // c00 c01 c02 0
@@ -57,9 +57,9 @@ void cramer::inverse (const float (& m) [3] [4], float (& minv) [3] [4])
   v4f c1t = _mm_unpackhi_ps (c0s, c1s); // c01 c11 0   0
 
   // Inverse of m (transposed cofactor matrix divided by determinant).
-  store4f (minv [0], rdet * _mm_shuffle_ps (c0t, c2s, SHUFFLE (2, 3, 1, 3))); // rdet * (c00 c10 c20 0)
-  store4f (minv [1], rdet * _mm_shuffle_ps (c1t, c2s, SHUFFLE (0, 1, 2, 3))); // rdet * (c01 c11 c21 0)
-  store4f (minv [2], rdet * _mm_shuffle_ps (c0t, c2s, SHUFFLE (0, 1, 0, 3))); // rdet * (c02 c12 c22 0)
+  store4f (minv [0], rdet * _mm_shuffle_ps (c0t, c2s, SHUFFLE (2, 3, 1, 3)));
+  store4f (minv [1], rdet * _mm_shuffle_ps (c1t, c2s, SHUFFLE (0, 1, 2, 3)));
+  store4f (minv [2], rdet * _mm_shuffle_ps (c0t, c2s, SHUFFLE (0, 1, 0, 3)));
 }
 
 #endif
